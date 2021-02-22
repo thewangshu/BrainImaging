13ed5526f33d50bfc20b1d956be02d38c23a8245
thewangshu <78582550+thewangshu@users.noreply.github.com>
2021-02-22 16:04:12 +0530

    Update Assignment_2.py

--- Assignments/Assignment_2/Assignment_2.py
+++ Assignments/Assignment_2/Assignment_2.py
@@ -20,37 +20,37 @@ plt.show()
 #Plotting Histogram of Reverse_GS.png
 plt.hist(img_array, bins=10)
 
-#Performing Gaussian Filtering with sigma = 5
+#Performing Gaussian Filtering with sigma = 5 and plotting a histogram of the same
 sigma_05 = ndimage.gaussian_filter(img_array, sigma = 5)
 plt.imshow(sigma_05)
 plt.show()
 plt.hist(sigma_05, bins=10)
 
-#Performing Gaussian Filtering with sigma = 10
+#Performing Gaussian Filtering with sigma = 10 and plotting a histogram of the same
 sigma_10 = ndimage.gaussian_filter(img_array, sigma = 10)
 plt.imshow(sigma_10)
 plt.show()
 plt.hist(sigma_10, bins=10)
 
-#Performing Gaussian Filtering with sigma = 20
+#Performing Gaussian Filtering with sigma = 20 and plotting a histogram of the same
 sigma_20 = ndimage.gaussian_filter(img_array, sigma = 20)
 plt.imshow(sigma_20)
 plt.show()
 plt.hist(sigma_20, bins=10)
 
-#Performing Gaussian Filtering with sigma = 30
+#Performing Gaussian Filtering with sigma = 30 and plotting a histogram of the same
 sigma_30 = ndimage.gaussian_filter(img_array, sigma = 30)
 plt.imshow(sigma_30)
 plt.show()
 plt.hist(sigma_30, bins=10)
 
-#Performing Gaussian Filtering with sigma = 40
+#Performing Gaussian Filtering with sigma = 40 and plotting a histogram of the same
 sigma_40 = ndimage.gaussian_filter(img_array, sigma = 40)
 plt.imshow(sigma_40)
 plt.show()
 plt.hist(sigma_40, bins=10)
 
-#Performing Gaussian Filtering with sigma = 50
+#Performing Gaussian Filtering with sigma = 50 and plotting a histogram of the same
 sigma_50 = ndimage.gaussian_filter(img_array, sigma = 50)
 plt.imshow(sigma_50)
 plt.show()
