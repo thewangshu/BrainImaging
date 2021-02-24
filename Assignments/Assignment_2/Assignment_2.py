f1897a2071280a3495deac29b970ded0c4f03840
thewangshu <78582550+thewangshu@users.noreply.github.com>
2021-02-24 13:18:59 +0530

    Update Assignment_2.py

--- Assignments/Assignment_2/Assignment_2.py
+++ Assignments/Assignment_2/Assignment_2.py
@@ -1,5 +1,8 @@
 #Importing necessary libraries
 import matplotlib.pyplot as plt
+import numpy as np
+
+#Importing libraries necessary for Gaussian Smoothing
 from scipy import ndimage
 
 #Reading given image ("Brain.jpg") into array
@@ -16,39 +19,53 @@ plt.show()
 
 #Plotting Histogram of Reverse_GS.png
 plt.hist(img_array, bins=10)
+plt.title("Reverse Colour Greyscale")
+plt.show()
 
-#Performing Gaussian Filtering with sigma = 5 and plotting a histogram of the same
+#Performing Gaussian Filtering with sigma = 5
 sigma_05 = ndimage.gaussian_filter(img_array, sigma = 5)
 plt.imshow(sigma_05)
 plt.show()
 plt.hist(sigma_05, bins=10)
+plt.title("Sigma = 5")
+plt.show()
 
-#Performing Gaussian Filtering with sigma = 10 and plotting a histogram of the same
+#Performing Gaussian Filtering with sigma = 10
 sigma_10 = ndimage.gaussian_filter(img_array, sigma = 10)
 plt.imshow(sigma_10)
 plt.show()
 plt.hist(sigma_10, bins=10)
+plt.title("Sigma = 10")
+plt.show()
 
-#Performing Gaussian Filtering with sigma = 20 and plotting a histogram of the same
+#Performing Gaussian Filtering with sigma = 20
 sigma_20 = ndimage.gaussian_filter(img_array, sigma = 20)
 plt.imshow(sigma_20)
 plt.show()
 plt.hist(sigma_20, bins=10)
+plt.title("Sigma = 20")
+plt.show()
 
-#Performing Gaussian Filtering with sigma = 30 and plotting a histogram of the same
+#Performing Gaussian Filtering with sigma = 30
 sigma_30 = ndimage.gaussian_filter(img_array, sigma = 30)
 plt.imshow(sigma_30)
 plt.show()
 plt.hist(sigma_30, bins=10)
+plt.title("Sigma = 30")
+plt.show()
 
-#Performing Gaussian Filtering with sigma = 40 and plotting a histogram of the same
+#Performing Gaussian Filtering with sigma = 40
 sigma_40 = ndimage.gaussian_filter(img_array, sigma = 40)
 plt.imshow(sigma_40)
 plt.show()
 plt.hist(sigma_40, bins=10)
+plt.title("Sigma = 40")
+plt.show()
 
-#Performing Gaussian Filtering with sigma = 50 and plotting a histogram of the same
+#Performing Gaussian Filtering with sigma = 50
 sigma_50 = ndimage.gaussian_filter(img_array, sigma = 50)
 plt.imshow(sigma_50)
 plt.show()
 plt.hist(sigma_50, bins=10)
+plt.title("Sigma = 50")
+plt.show()
